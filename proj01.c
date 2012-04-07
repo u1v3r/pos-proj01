e3f8e335db29640b821ba4ef5a03e7cc3f5c83d6
Radovan Dvorsky <rdvorsky6@gmail.com>
2012-04-07 20:52:47 +0200

    implementacia SIGUSR1

--- proj01.c
+++ proj01.c
@@ -1,7 +1,107 @@
-#include "signal.h"
+#define _POSIX_SOURCE
+
+#define ABC_START 65
+#define ABC_END 91
+
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+volatile sig_atomic_t signaled;
+volatile sig_atomic_t reset = 0;
+
+void sig_usr1(int sig){
+
+    signaled = 1;
+}
+
+void sig_usr2(int sig){
+
+    reset = 1;
+    printf("%d:id %d\n",sig,getpid());
+}
 
 int main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+    pid_t pid,ppid;
+    struct sigaction sigact1;
+    struct sigaction sigact2;
+    sigset_t empty_set;
+    sigset_t sigusr2_set;
+    int char_num = ABC_START;
+    int c;
+
+    ppid = getpid();
+
+    sigemptyset(&empty_set);
+
+    sigemptyset(&sigusr2_set);
+    sigaddset(&sigusr2_set,SIGUSR2);
+
+    sigact1.sa_handler = sig_usr1;
+    sigact1.sa_flags = 0;
+    sigact2.sa_handler = sig_usr2;
+    sigact2.sa_flags = 0;
+
+    if (sigaction(SIGUSR1,&sigact1,NULL)) {
+		perror("sigaction()");
+		return(1);
+	}
+    if (sigaction(SIGUSR2,&sigact2,NULL)) {
+		perror("sigaction()");
+		return(1);
+	}
+
+    /* fork */
+    if((pid = fork()) < 0) {
+        perror("fork error");
+    }
+
+    /* parrent process */
+    if(pid > 0){
+        while (1){
+
+
+            if(char_num == ABC_END) char_num = ABC_START;
+            printf("Parent (%d): '%c'\n",ppid,char_num++);
+            kill(pid, SIGUSR1);
+
+            /* pockaj na singal USR1 a ignoruj USR2 */
+            if(!signaled) sigsuspend(&sigusr2_set);
+            signaled = 0;
+
+            /* len enter */
+            printf("Press enter...");
+            c = getchar();
+            while (c != '\n' && c != EOF) c=getchar();
+
+            /* DOPRACUJ USR2 */
+            /*sigprocmask(SIG_BLOCK, &sigusr2_set,NULL);*/
+            /*sigprocmask(SIG_UNBLOCK, &sigusr2_set,NULL);*/
+        }
+    }
+
+
+    /* child process */
+    if(pid == 0) {
+        while (1){
+
+            /* pockaj na singal USR1 a ignoruj USR2 */
+            if(!signaled) sigsuspend(&sigusr2_set);
+
+            signaled = 0;
+            if(char_num == ABC_END) char_num = ABC_START;
+            printf("Child (%d): '%c'\n",getpid(),char_num++);
+            kill(ppid, SIGUSR1);
+
+
+
+            /* DOPRACUJ USR2 */
+            /*sigprocmask(SIG_BLOCK, &sigusr2_set,NULL);*/
+            /*sigprocmask(SIG_UNBLOCK, &sigusr2_set,NULL);*/
+        }
+    }
+
+	return 0;
 }
